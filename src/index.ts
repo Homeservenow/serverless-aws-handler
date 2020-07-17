5099aeb084fa1fe1efc835bc735f53e412bf9736
bashleigh <ashleighsimonelli@gmail.com>
2020-07-17 10:45:44 +0100

    chore: clean up

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,4 @@ export * from "./http.response.handler";
 export * from "./http.handler";
 export * from "./exceptions";
 export { ErrorHandlingOptionsType, LoggerInterface } from "./logging.handler";
-export {isHttpErrorException} from './utils';
+export { isHttpErrorException } from "./utils";

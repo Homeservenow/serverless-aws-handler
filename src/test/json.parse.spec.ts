2f0756fae06cdb8a13e8c846c38066c13913d635
bashleigh <ashleighsimonelli@gmail.com>
2020-07-17 17:33:45 +0100

    chore: linting

--- src/test/json.parse.spec.ts
+++ src/test/json.parse.spec.ts
@@ -3,15 +3,16 @@ import { HttpErrorException } from "./../exceptions";
 import { HttpStatusCode } from "./../enum";
 import { createMockAPIGatewayEvent } from "./events";
 
-
-describe('JSONParse', () => {
-  it('Can receive malformed exception', () => {
+describe("JSONParse", () => {
+  it("Can receive malformed exception", () => {
     let exception;
     try {
-      JSONParse(createMockAPIGatewayEvent({
-        body: '["test" : 123]',
-      }));
-    } catch(e) {
+      JSONParse(
+        createMockAPIGatewayEvent({
+          body: '["test" : 123]',
+        }),
+      );
+    } catch (e) {
       exception = e;
     }
     expect(exception.status).toBe(HttpStatusCode.BAD_REQUEST);

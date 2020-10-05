1e93cb40fc6a0734f84c76680ce305984d4891a9
bashleigh <ashleighsimonelli@gmail.com>
2020-10-05 16:53:41 +0100

    fix: generic typing

--- src/sqs/sqs.serialise.ts
+++ src/sqs/sqs.serialise.ts
@@ -2,4 +2,5 @@ import { SQSRecord } from "aws-lambda";
 
 export type SqsSerialiseFunction<R extends any> = (record: SQSRecord) => R;
 
-export const serialise = (record: SQSRecord) => JSON.parse(record.body) as any;
+export const serialise: SqsSerialiseFunction<any> = (record: SQSRecord): any =>
+  JSON.parse(record.body) as any;
